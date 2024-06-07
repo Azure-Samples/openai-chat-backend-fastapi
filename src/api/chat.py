5f13b24cfdb35c39ddec69ee2386973b08b1154a
Pamela Fox <pamela.fox@gmail.com>
2024-06-07 11:13:59 -0700

    Upgrade to latest Chat Protocol

--- src/api/chat.py
+++ src/api/chat.py
@@ -16,31 +16,41 @@ class Message(pydantic.BaseModel):
 
 class ChatRequest(pydantic.BaseModel):
     messages: list[Message]
-    stream: bool = True
+
+
+SYSTEM_PROMPT = """You are a helpful assistant."""
 
 
 @router.post("/chat")
-async def chat_handler(chat_request: ChatRequest):
-    messages = [{"role": "system", "content": "You are a helpful assistant."}] + chat_request.messages
+async def chat_handler(chat_request: ChatRequest) -> dict:
+    messages = [{"role": "system", "content": SYSTEM_PROMPT}] + chat_request.messages
     # Azure Open AI takes the deployment name as the model name
     model = os.getenv("AZURE_OPENAI_CHATGPT_DEPLOYMENT", "chatgpt")
 
-    if chat_request.stream:
+    response = await clients["openai"].chat.completions.create(
+        model=model,
+        messages=messages,
+        stream=False,
+    )
+    first_choice = response.model_dump()["choices"][0]
+    return {"message": first_choice["message"]}
 
-        async def response_stream():
-            chat_coroutine = clients["openai"].chat.completions.create(
-                model=model,
-                messages=messages,
-                stream=True,
-            )
-            async for event in await chat_coroutine:
-                yield json.dumps(event.model_dump(), ensure_ascii=False) + "\n"
 
-        return fastapi.responses.StreamingResponse(response_stream())
-    else:
-        response = await clients["openai"].chat.completions.create(
+@router.post("/chat/stream")
+async def chat_stream_handler(chat_request: ChatRequest) -> fastapi.responses.StreamingResponse:
+    messages = [{"role": "system", "content": SYSTEM_PROMPT}] + chat_request.messages
+    # Azure Open AI takes the deployment name as the model name
+    model = os.getenv("AZURE_OPENAI_CHATGPT_DEPLOYMENT", "chatgpt")
+
+    async def response_stream():
+        chat_coroutine = clients["openai"].chat.completions.create(
             model=model,
             messages=messages,
-            stream=False,
+            stream=True,
         )
-        return response.model_dump()
+        async for event in await chat_coroutine:
+            if event.choices:
+                first_choice = event.model_dump()["choices"][0]
+                yield json.dumps({"delta": first_choice["delta"]}, ensure_ascii=False) + "\n"
+
+    return fastapi.responses.StreamingResponse(response_stream())
